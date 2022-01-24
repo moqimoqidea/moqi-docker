e9e8094bd43d2eabe3c4e4d3e86a539c9581635f
moqi <moqimoqidea@gmail.com>
2022-01-24 16:03:16 +0800

    Update

--- shell/build.sh
+++ shell/build.sh
@@ -4,18 +4,18 @@
 docker build -f ../centos/Dockerfile -t moqi-centos .
 
 if [ $? -ne 0 ]; then
-  echo -e "\n************************* build moqi-centos failed *************************\n"
+  echo "\n************************* build moqi-centos failed *************************\n"
   exit 1
 fi
 
-echo -e "\n************************* build moqi-centos success *************************\n"
+echo "\n************************* build moqi-centos success *************************\n"
 
 # build moqi-ubuntu
 docker build -f ../ubuntu/Dockerfile -t moqi-ubuntu .
 
 if [ $? -ne 0 ]; then
-  echo -e "\n************************* build moqi-ubuntu failed *************************\n"
+  echo "\n************************* build moqi-ubuntu failed *************************\n"
   exit 1
 fi
 
-echo -e "\n************************* build moqi-ubuntu success *************************\n"
+echo "\n************************* build moqi-ubuntu success *************************\n"
