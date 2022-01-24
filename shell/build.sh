25255ee9dc853cd5f0c8727f9cc428c1814ca0e4
moqi <moqimoqidea@gmail.com>
2022-01-24 15:22:18 +0800

    Update

--- shell/build.sh
+++ shell/build.sh
@@ -1,1 +1,11 @@
 #!/bin/sh
+
+# build moqi-centos
+docker build -f ../centos/Dockerfile -t moqi-centos .
+
+echo "build moqi-centos success"
+
+# build moqi-ubuntu
+docker build -f ../ubuntu/Dockerfile -t moqi-ubuntu .
+
+echo "build moqi-ubuntu success"
