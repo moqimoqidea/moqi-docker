63ca26752809e2af74781bb856c413f77c96ff02
moqi <moqimoqidea@gmail.com>
2022-01-24 22:49:17 +0800

    Update build.sh

--- shell/build.sh
+++ shell/build.sh
@@ -1,21 +1,60 @@
 #!/bin/sh
 
-# build moqi-centos
-docker build -f ../centos/Dockerfile -t moqi-centos .
+# get timestamp
+nowTimestamp=$(date '+%Y%m%d%H%M%S')
+
+# get mac os version
+macOsFullName=$(uname -a)
+
+
+# shellcheck disable=SC2039
+if [[ $macOsFullName == *"arm"* ]]; then
+  tag="arm"
+else
+  tag="x86"
+fi
+
+fullTag=$tag"-"$nowTimestamp
+fullCentOs="moqimoqidea/centos:$fullTag"
+fullUbuntu="moqimoqidea/ubuntu:$fullTag"
+
+
+
+# centos
+docker build -f ../centos/Dockerfile -t "$fullCentOs" .
 
 if [ $? -ne 0 ]; then
-  echo "\n************************* build moqi-centos failed *************************\n"
+  printf "\n************************* build %s failed *************************\n" "$fullCentOs"
   exit 1
 fi
 
-echo "\n************************* build moqi-centos success *************************\n"
+printf "\n************************* build %s success *************************\n" "$fullCentOs"
 
-# build moqi-ubuntu
-docker build -f ../ubuntu/Dockerfile -t moqi-ubuntu .
+docker push "$fullCentOs"
 
 if [ $? -ne 0 ]; then
-  echo "\n************************* build moqi-ubuntu failed *************************\n"
+  printf "\n************************* push %s failed *************************\n" "$fullCentOs"
   exit 1
+else
+  printf "\n************************* push %s success *************************\n" "$fullCentOs"
 fi
 
-echo "\n************************* build moqi-ubuntu success *************************\n"
+
+# ubuntu
+docker build -f ../ubuntu/Dockerfile -t "$fullUbuntu" .
+
+if [ $? -ne 0 ]; then
+  printf "\n************************* build %s failed *************************\n" "$fullUbuntu"
+  exit 1
+else
+  printf "\n************************* build %s success *************************\n" "$fullUbuntu"
+fi
+
+docker push "$fullUbuntu"
+
+if [ $? -ne 0 ]; then
+  printf "\n************************* push %s failed *************************\n" "$fullUbuntu"
+  exit 1
+else
+  printf "\n************************* push %s success *************************\n" "$fullUbuntu"
+fi
